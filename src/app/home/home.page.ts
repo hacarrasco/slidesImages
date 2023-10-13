20103f95692395b2afcc04de2ed5239ab7e77138
hacarrasco <Hugo.aCasstillo@gmail.com>
2023-10-13 07:38:35 -0600

    fixed logic and styles

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-rick-and-morty',
@@ -9,20 +10,26 @@ import { HttpClient } from '@angular/common/http';
 export class HomePage {
   currentImageUrl!: string;
   loading: boolean = false;
+  showSpinner = true;
+  showButton = true;
 
-  constructor(private http: HttpClient) {}
+  constructor(private navCtrl: NavController, private http: HttpClient) {}
 
   loadAndShowImages() {
     this.loading = true;
 
     // Llama a la API de Rick and Morty para obtener imágenes
-    this.http.get<any>('https://rickandmortyapi.com/api/character/')
-      .subscribe(response => {
-        const images = response.results.map((character: { image: any; }) => character.image);
-        
+    this.http
+      .get<any>('https://rickandmortyapi.com/api/character/')
+      .subscribe((response) => {
+        const images = response.results.map(
+          (character: { image: any }) => character.image
+        );
+
         // Muestra la primera imagen
         this.currentImageUrl = images[0];
-
+        this.showSpinner = false;
+        this.showButton = false;
         // Cambia la imagen cada 5 segundos (5000 ms)
         let index = 1;
         const interval = setInterval(() => {
@@ -32,17 +39,10 @@ export class HomePage {
           } else {
             clearInterval(interval);
             this.loading = false;
+            this.navCtrl.navigateForward('/end-slider')
           }
         }, 1000);
       });
-      this.loading = false;
+    this.loading = false;
   }
 }
-
-
-
-
-
-
-
-
